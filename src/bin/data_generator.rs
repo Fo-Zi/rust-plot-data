054a9cce92a72db5d07fa02cefedbe4c6eaa493e
Fo-Zi <facundozingarelli@gmail.com>
2024-12-31 19:03:18 -0300

    Minor adjustments

--- src/bin/data_generator.rs
+++ src/bin/data_generator.rs
@@ -3,13 +3,16 @@ use csv::{Writer};
 use serde::{Serialize};
 use chrono::{format, DateTime, TimeZone, Utc};
 use rand::{Rng};
+use std::f64::consts::PI;
 
+// Global const parameters
 const TIMESTAMP_STEP_IN_HOURS: u32 = 1;
 const NUMBER_OF_POINTS: u32 = 5000;
 const MAX_DATA_VALUE: i32 = 50;
 const MIN_DATA_VALUE: i32 = -30 ;
-const FILE_PATH: &str =  "csv_test.txt";
+const FILE_PATH: &str =  "csv_test.csv";
 
+// For readability
 #[macro_export]
 macro_rules! format_datetime {
     ($dt:expr) => {
@@ -17,30 +20,76 @@ macro_rules! format_datetime {
     };
 }
 
+// The record we will use to write onto the csv
 #[derive(Serialize)]
 struct Record {
     timestamp: String,
-    value: i32,
+    value: f32,
+}
+
+// Static sine lookup table for common angles in degrees.
+const SINE_LOOKUP_TABLE: [f32; 36] = [
+    0.0, // 0 degrees
+    0.17364817766693033, // 10 degrees
+    0.3420201433256687, // 20 degrees
+    0.49999999999999994, // 30 degrees
+    0.6427876096865393, // 40 degrees
+    0.766044443118978, // 50 degrees
+    0.8660254037844387, // 60 degrees
+    0.9396926207859084, // 70 degrees
+    0.984807753012208, // 80 degrees
+    1.0, // 90 degrees
+    0.984807753012208, // 100 degrees
+    0.9396926207859084, // 110 degrees
+    0.8660254037844387, // 120 degrees
+    0.766044443118978, // 130 degrees
+    0.6427876096865393, // 140 degrees
+    0.49999999999999994, // 150 degrees
+    0.3420201433256687, // 160 degrees
+    0.17364817766693033, // 170 degrees
+    0.0, // 180 degrees
+    -0.17364817766693033, // 190 degrees
+    -0.3420201433256687, // 200 degrees
+    -0.49999999999999994, // 210 degrees
+    -0.6427876096865393, // 220 degrees
+    -0.766044443118978, // 230 degrees
+    -0.8660254037844387, // 240 degrees
+    -0.9396926207859084, // 250 degrees
+    -0.984807753012208, // 260 degrees
+    -1.0, // 270 degrees
+    -0.984807753012208, // 280 degrees
+    -0.9396926207859084, // 290 degrees
+    -0.8660254037844387, // 300 degrees
+    -0.766044443118978, // 310 degrees
+    -0.6427876096865393, // 320 degrees
+    -0.49999999999999994, // 330 degrees
+    -0.3420201433256687, // 340 degrees
+    -0.17364817766693033, // 350 degrees
+];
+
+fn truncate_to_two_decimal_places(value: f32) -> f32 {
+    (value * 100.0).trunc() / 100.0
 }
 
 fn main() {
+    
+    // Arbitrary date as a "seed" to generate the time series
     let start_date_time = Utc.with_ymd_and_hms(2020, 1, 1, 0, 1, 1).unwrap();
-    let formatted = format_datetime!(start_date_time);
-    assert_eq!(formatted, "2020-01-01 00:01:01");
-
     let start_timestamp = start_date_time.timestamp();
+    
+    // Instantiating random number generator 
     let mut rng = rand::thread_rng();
 
+    // Instantiating csv writer from file
     let mut writer = Writer::from_path(FILE_PATH).unwrap();
 
-    // Write the headers
-    //writer.write_record(&["Timestamp", "Value"]).unwrap();
-
+    // Data generation 
     for i in 0..NUMBER_OF_POINTS {
         let timestamp = start_timestamp + (i*TIMESTAMP_STEP_IN_HOURS*60*60) as i64 ;
+        let value = truncate_to_two_decimal_places(rng.gen_range(MIN_DATA_VALUE..MAX_DATA_VALUE) as f32 + 30_f32*SINE_LOOKUP_TABLE[i as usize % 36]);
         let record = Record{
             timestamp: format_datetime!(DateTime::from_timestamp(timestamp,0).unwrap()),
-            value: rng.gen_range(MIN_DATA_VALUE..MAX_DATA_VALUE),
+            value: value ,
         };
         writer.serialize(record).unwrap();
     }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,51 @@
 use std::error::Error;
+use serde::{Serialize};
 use plotly::common::Mode;
 use plotly::{ImageFormat, Plot, Scatter};
-use chrono::{NaiveDate, Duration};
+use chrono::{};
+
+// The record we will use to write onto the csv
+#[derive(Serialize,Clone)]
+struct Record {
+    timestamp: String,
+    value: i32,
+}
+
+struct FirFilter{
+    last_value: f32,
+    alpha: f32,
+}
+
+impl FirFilter {
+    pub fn filter_new_value(&mut self,new_value: f32) -> f32 {
+        self.last_value = self.alpha * (self.last_value) + (self.alpha - 1_f32)* (new_value);
+        return self.last_value;
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>>  {
-    // Generate example time series data
-    let start_date = NaiveDate::from_ymd_opt(2024, 1, 1).unwrap();
-    let dates: Vec<String> = (0..10)
-        .map(|i| (start_date + Duration::days(i)).to_string())
-        .collect();
-    let values: Vec<f64> = (0..10).map(|i| (i as f64).sin()).collect();
-
-    // Create a scatter plot for the time series data
-    let trace = Scatter::new(dates, values)
-        .name("Time Series")
-        .mode(Mode::LinesMarkers);
-
-    // Build the plot
+    
+    // Initialize the plot
     let mut plot = Plot::new();
+    let mut x_values: Vec<String> = vec![];
+    let mut y_values: Vec<i32> = vec![];
+
+    // Read the data (For now the data is read at once and then processed, this will be done concurrently later on)
+
+    // Process the data
+    let mut filter = FirFilter{
+        last_value: 0_f32,
+        alpha: 0.9
+    };
+
+    // Create an empty Scatter trace
+    let trace = Scatter::new(x_values, y_values)
+        .name("Dynamic Data")
+        .mode(Mode::LinesMarkers);
     plot.add_trace(trace);
-    //plot.set_title("Time Series Plot");
+
+    // Launch the plot viewer
     plot.show();
+    
     Ok(())
 }
